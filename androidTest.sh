a0231f86e4550d8e689896f47bb439522cb70391
Margarita Volodina <volodina@surfstudio.co>
2018-11-13 16:52:24 +0300

    Params for launch were added

--- androidTest.sh
+++ androidTest.sh
@@ -5,9 +5,15 @@ EMULATOR_STATUS=`adb devices | grep emulator | cut -f1`
 if [[ -z "$EMULATOR_STATUS" ]]; then
     # read params from config file
     . avd-config
-    rm -r /home/volodina/.android/avd/avd-androidTest.* #todo remove
     avdmanager create avd -f -n "$avd_name" -d "$device_name" -k "$sdk_id" -c "$sdcard_size"
-    emulator -avd "$avd_name" -skin "$scin_size" -no-snapshot-save
+    # launch emulator in background process
+    # emulator -avd "$avd_name" -skin "$scin_size" -no-snapshot-save &
+    # launch emulator in another terminal tab
+    #gnome-terminal -x sh -c "emulator -avd "$avd_name" -skin "$scin_size" -no-snapshot-save"
+    # launch emulator in another terminal window
+    gnome-terminal -e "emulator -avd "$avd_name" -skin "$scin_size" -no-snapshot-save"
+    adb wait-for-device
+    echo yes
 else
     echo Emulator is running
 fi
