1eb7086d4ce878fd7b89854fa2367f8a65133449
Margarita Volodina <volodina@surfstudio.co>
2018-11-13 18:18:17 +0300

    Primary launch of instrumental tests was added

--- androidTest.sh
+++ androidTest.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# build APK for instrumental tests
+#todo cd to project directory
+#./gradlew assembleAndroidTest
+#grep -r --include "*-androidTest.apk" . | cut -d ' ' -f3
+
 # check if the emulator is running
 EMULATOR_STATUS=`adb devices | grep emulator | cut -f1`
 if [[ -z "$EMULATOR_STATUS" ]]; then
@@ -9,11 +14,16 @@ if [[ -z "$EMULATOR_STATUS" ]]; then
     # launch emulator in background process
     # emulator -avd "$avd_name" -skin "$scin_size" -no-snapshot-save &
     # launch emulator in another terminal tab
-    #gnome-terminal -x sh -c "emulator -avd "$avd_name" -skin "$scin_size" -no-snapshot-save"
+    # gnome-terminal -x sh -c "emulator -avd "$avd_name" -skin "$scin_size" -no-snapshot-save"
     # launch emulator in another terminal window
     gnome-terminal -e "emulator -avd "$avd_name" -skin "$scin_size" -no-snapshot-save"
-    adb wait-for-device
-    echo yes
+    adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done;'
+    adb push /home/volodina/AndroidStudioProjects/android-standard/app-migration-sample/build/outputs/apk/debug/app-migration-sample-debug.apk /data/local/tmp/ru.surfstudio.android.app.migration.sample
+    adb shell pm install -t -r "/data/local/tmp/ru.surfstudio.android.app.migration.sample"
+    adb push /home/volodina/AndroidStudioProjects/android-standard/app-migration-sample/build/outputs/apk/androidTest/debug/app-migration-sample-debug-androidTest.apk /data/local/tmp/ru.surfstudio.android.app.migration.sample.test
+    adb shell pm install -t -r "/data/local/tmp/ru.surfstudio.android.app.migration.sample.test"
+    adb shell am instrument -w -r -e debug false -e class 'ru.surfstudio.android.app.migration.sample.AppMigrationSampleTest' ru.surfstudio.android.app.migration.sample.test/androidx.test.runner.AndroidJUnitRunner
+    #todo close emulator
 else
     echo Emulator is running
 fi
