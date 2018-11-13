383a8d45d5ad12359f8a6bd55f572947da5a4336
Margarita <margo.himera@yandex.ru>
2018-11-13 23:26:19 +0300

    Functions were added

--- androidTest.sh
+++ androidTest.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
 
+'''
+    Функция, возвращающая список имен APK-файлов с заданным суффиксом,
+    который передается параметром.
+'''
+get_apk_list() {
+    grep -r --include "*-$1.apk" . | cut -d ' ' -f3
+}
+
+print() {
+    echo $1
+    echo
+    echo
+    echo
+}
+
+push() {
+    adb push $1 $2
+}
+
+install_apk() {
+    adb shell pm install -t -r $1
+}
+
 # build APK for instrumental tests
 #todo cd to project directory
 #./gradlew assembleAndroidTest
-#grep -r --include "*-androidTest.apk" . | cut -d ' ' -f3
+#ANDROID_TEST_APK_LIST=`get_apk_list "androidTest"`
+#print $ANDROID_TEST_APK_LIST
+#DEBUG_APK_LIST=`get_apk_list "debug"`
+#print $DEBUG_APK_LIST
+
+ANDROID_JUNIT_RUNNER_NAME="androidx.test.runner.AndroidJUnitRunner"
 
 # check if the emulator is running
 EMULATOR_STATUS=`adb devices | grep emulator | cut -f1`
@@ -16,13 +44,18 @@ if [[ -z "$EMULATOR_STATUS" ]]; then
     # launch emulator in another terminal tab
     # gnome-terminal -x sh -c "emulator -avd "$avd_name" -skin "$scin_size" -no-snapshot-save"
     # launch emulator in another terminal window
+    #todo add `adb -s $name shell ...`
     gnome-terminal -e "emulator -avd "$avd_name" -skin "$scin_size" -no-snapshot-save"
     adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done;'
-    adb push /home/volodina/AndroidStudioProjects/android-standard/app-migration-sample/build/outputs/apk/debug/app-migration-sample-debug.apk /data/local/tmp/ru.surfstudio.android.app.migration.sample
-    adb shell pm install -t -r "/data/local/tmp/ru.surfstudio.android.app.migration.sample"
-    adb push /home/volodina/AndroidStudioProjects/android-standard/app-migration-sample/build/outputs/apk/androidTest/debug/app-migration-sample-debug-androidTest.apk /data/local/tmp/ru.surfstudio.android.app.migration.sample.test
-    adb shell pm install -t -r "/data/local/tmp/ru.surfstudio.android.app.migration.sample.test"
-    adb shell am instrument -w -r -e debug false -e class 'ru.surfstudio.android.app.migration.sample.AppMigrationSampleTest' ru.surfstudio.android.app.migration.sample.test/androidx.test.runner.AndroidJUnitRunner
+
+    #todo try to change /data/local/tmp/... to custom package
+    push /home/volodina/AndroidStudioProjects/android-standard/app-migration-sample/build/outputs/apk/debug/app-migration-sample-debug.apk /data/local/tmp/ru.surfstudio.android.app.migration.sample
+    install_apk "/data/local/tmp/ru.surfstudio.android.app.migration.sample"
+
+    push /home/volodina/AndroidStudioProjects/android-standard/app-migration-sample/build/outputs/apk/androidTest/debug/app-migration-sample-debug-androidTest.apk /data/local/tmp/ru.surfstudio.android.app.migration.sample.test
+    install_apk "/data/local/tmp/ru.surfstudio.android.app.migration.sample.test"
+
+    adb shell am instrument -w -r -e debug false -e class 'ru.surfstudio.android.app.migration.sample.AppMigrationSampleTest' ru.surfstudio.android.app.migration.sample.test/"$ANDROID_JUNIT_RUNNER_NAME"
     #todo close emulator
 else
     echo Emulator is running
